bd4a7dea42d5b21940551c70817703950d208bcc
reynoldadade <reynoldadade@gmail.com>
2022-06-04 16:02:07 +0000

    refactored to composition api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,204 @@
-<script>
+<script setup>
+import { watch, ref, computed, onMounted } from "vue";
 import mapboxgl from "mapbox-gl";
 import getDistance from "@turf/distance";
-
 import {
   uniqueNamesGenerator,
   adjectives,
   animals,
 } from "unique-names-generator";
+
+const map = ref(null);
+const coordinates = ref([]);
+const maxDistance = ref(0);
+const groupColors = ref([]);
+
+watch(
+  coordinates,
+  (newCoordinates) => {
+    if (newCoordinates.length > 8) {
+      generateColors(groupCordinates.value);
+      drawLines(groupCordinates.value);
+    }
+  },
+  { deep: true }
+);
+
+function createCoordinates(coords) {
+  const { lng, lat } = coords;
+  const el = document.createElement("div");
+  el.className = "marker";
+
+  const marker = new mapboxgl.Marker(el).setLngLat([lng, lat]).addTo(map.value);
+  coordinates.value.push({
+    lng,
+    lat,
+    name: uniqueNamesGenerator({
+      dictionaries: [adjectives, animals],
+      separator: " ",
+    }),
+    marker,
+  });
+
+  calculateMaxDistance(coords);
+}
+
+// function threeDp(item) {
+//   //3 decimal places
+//   return item.value.toFixed(3);
+// }
+
+function calculateMaxDistance(point) {
+  //calculate distance between coord and each existing point for max distance
+  const { lng, lat } = point;
+  let max = 0;
+  coordinates.value.forEach((coord) => {
+    const distance = getDistance([lng, lat], [coord.lng, coord.lat]);
+
+    if (distance > max) {
+      max = distance;
+    }
+  });
+  maxDistance.value = max * 0.25;
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function generateColors(group) {
+  console.log(group);
+  if (groupColors.value.length > group.length) {
+    groupColors.value.splice(
+      group.length,
+      groupColors.value.length - group.length
+    );
+  } else {
+    const diff = group.length - groupColors.value.length;
+    for (let i = 0; i < diff; i++) {
+      groupColors.value.push(randomColor());
+    }
+  }
+}
+
+function changeColor(index) {
+  const randomColor = randomColor();
+  if (groupColors.value.includes(randomColor)) {
+    return changeColor(index);
+  }
+  groupColors.value[index] = randomColor;
+}
+
+function generateMapLines(groupCordinates) {
+  const mapLines = [];
+  groupCordinates.forEach((coords) => {
+    const { group } = coords;
+    console.log(group);
+    const line = {
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        coordinates: [group.map((coord) => [coord.lng, coord.lat])],
+      },
+    };
+
+    mapLines.push(line);
+  });
+  return mapLines;
+}
+
+function drawLines(groupCordinates) {
+  const mapLines = generateMapLines(groupCordinates);
+
+  map.value.addSource("lines", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: mapLines,
+    },
+  });
+  map.value.addLayer({
+    id: "lines",
+    type: "line",
+    source: "lines",
+    paint: {
+      "line-width": 2,
+      "line-color": "#000",
+    },
+  });
+}
+
+// computed properties
+const groupCordinates = computed(() => {
+  let coordinateCopy = [...coordinates.value];
+  if (coordinateCopy.length === 0) {
+    return [];
+  }
+  if ((coordinateCopy.length > 0) & (coordinateCopy.length < 9)) {
+    return [
+      {
+        name: "",
+        group: coordinateCopy,
+      },
+    ];
+  }
+  let groups = [];
+
+  // loop through the coordinates and group them by distance
+  for (let i = 0; i < coordinateCopy.length; i++) {
+    const group = [];
+    for (let j = 0; j < coordinateCopy.length; j++) {
+      const distance = getDistance(
+        [coordinateCopy[i].lng, coordinateCopy[i].lat],
+        [coordinateCopy[j].lng, coordinateCopy[j].lat]
+      );
+      // if less than max distance then add to a group
+      if (distance < maxDistance.value) {
+        group.push(coordinateCopy[j]);
+      }
+    }
+    // if not empty add to groups
+    if (group.length > 0) {
+      // issue with what i have now is that there will be duplicates
+      group.sort((a, b) => a.name - b.name);
+      const groupObject = {
+        name: group.map((item) => item.name).toString(),
+        group,
+      };
+      //before pushing check if array already exists in groups
+      const exists = groups.find((item) => item.name === groupObject.name);
+      if (!exists) {
+        groups.push(groupObject);
+      }
+    }
+  }
+
+  return groups;
+});
+
+function threeDp(value) {
+  return value.toFixed(3);
+}
+
+onMounted(() => {
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoid2FsdWxlbGFkbWluIiwiYSI6ImNrMm5samZ3bzBya2QzY29maDh6czJ6bGcifQ.LRMREa0M12e8Cjdk7TCS6w";
+
+  map.value = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [-96, 37.8],
+    zoom: 9,
+  });
+
+  map.value.on("click", (e) => {
+    const coords = e.lngLat;
+    createCoordinates(coords);
+  });
+});
+</script>
+
+<script>
 export default {
   head() {
     return {
@@ -19,117 +211,38 @@ export default {
       ],
     };
   },
-
-  data() {
-    return {
-      map: null,
-      coordinates: [],
-      maxDistance: 0,
-    };
-  },
-  mounted() {
-    mapboxgl.accessToken =
-      "pk.eyJ1Ijoid2FsdWxlbGFkbWluIiwiYSI6ImNrMm5samZ3bzBya2QzY29maDh6czJ6bGcifQ.LRMREa0M12e8Cjdk7TCS6w";
-    this.map = new mapboxgl.Map({
-      container: "map", // container ID
-      style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 10, // starting zoom
-    });
-
-    this.map.on("click", (e) => {
-      this.createCoordinates(e.lngLat);
-    });
-  },
-  computed: {
-    groupCordinates() {
-      let coordinateCopy = [...this.coordinates];
-      let groups = [];
-
-      // loop through the coordinates and group them by distance
-      for (let i = 0; i < coordinateCopy.length; i++) {
-        const group = [];
-        for (let j = 0; j < coordinateCopy.length; j++) {
-          const distance = getDistance(
-            [coordinateCopy[i].lng, coordinateCopy[i].lat],
-            [coordinateCopy[j].lng, coordinateCopy[j].lat]
-          );
-          // if less than max distance then add to a group
-          if (distance <= this.maxDistance) {
-            group.push(coordinateCopy[j]);
-          }
-        }
-        // if not empty add to groups
-        if (group.length > 0) {
-          // issue with what i have now is that there will be duplicates
-          group.sort((a, b) => a.name - b.name);
-          const groupObject = {
-            name: group.map((item) => item.name).toString(),
-            group,
-          };
-          //before pushing check if array already exists in groups
-          const exists = groups.find((item) => item.name === groupObject.name);
-          if (!exists) {
-            groups.push(groupObject);
-          }
-        }
-      }
-      return groups;
-    },
-  },
-  methods: {
-    createCoordinates(coords) {
-      const { lng, lat } = coords;
-      const el = document.createElement("div");
-      el.className = "marker";
-
-      const marker = new mapboxgl.Marker(el)
-        .setLngLat([lng, lat])
-        .addTo(this.map);
-      this.coordinates.push({
-        lng,
-        lat,
-        name: uniqueNamesGenerator({
-          dictionaries: [adjectives, animals],
-          separator: " ",
-        }),
-        marker,
-      });
-
-      this.calculateMaxDistance(coords);
-    },
-    threeDp(value) {
-      //3 decimal places
-      return value.toFixed(3);
-    },
-    calculateMaxDistance(point) {
-      //calculate distance between coord and each existing point for max distance
-      const { lng, lat } = point;
-      let maxDistance = 0;
-      this.coordinates.forEach((coord) => {
-        const distance = getDistance([lng, lat], [coord.lng, coord.lat]);
-
-        if (distance > maxDistance) {
-          maxDistance = distance;
-        }
-      });
-      this.maxDistance = maxDistance * 0.25;
-    },
-  },
 };
 </script>
 
 <template>
   <div class="rounded-[32px] mapCard p-6 flex justify-between">
     <div class="w-[210px] flex flex-col justify-between">
-      <div class=" ">
+      <div class="flex-grow flex flex-col">
         <div class="mb-[14px] text-[20px] leading-[24px]">Groups</div>
-        <div>
-          <div v-for="(coord, index) in coordinates" :key="index">
-            {{ coord.name }}
-            <span class="text-[#767676]"
-              >{{ threeDp(coord.lng) }} {{ threeDp(coord.lat) }}</span
-            >
+        <div class="h-[579px] overflow-y-auto">
+          <div
+            v-for="(coord, index) in groupCordinates"
+            :key="index"
+            class="mb-4"
+          >
+            <button class="flex items-center gap-1" @click="changeColor(index)">
+              <div
+                class="h-3 w-3 rounded-[2px]"
+                :style="`background-color: ${groupColors[index]}`"
+                v-if="coord.name"
+              ></div>
+              <div class="flex h-full items-center pt-1">
+                {{ coord.name ? "Group" : "Ungrouped" }}
+              </div>
+            </button>
+            <div v-for="group in coord.group" :key="group.name">
+              <span>
+                {{ group.name }}
+              </span>
+              <span class="text-[#767676] pl-2"
+                >{{ threeDp(group.lng) }} {{ threeDp(group.lat) }}</span
+              >
+            </div>
           </div>
         </div>
       </div>
