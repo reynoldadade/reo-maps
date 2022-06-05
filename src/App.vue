976bb6e3accbff351df02f3d29a6ec524f837033
reynoldadade <reynoldadade@gmail.com>
2022-06-05 20:26:55 +0000

    fix: color not changing for 2 points

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,15 @@ import {
   adjectives,
   animals,
 } from "unique-names-generator";
-import { polygon } from "@turf/helpers";
+import { lineString, polygon } from "@turf/helpers";
+import GroupComponent from "./components/GroupComponent.vue";
 
 const map = ref(null);
 const coordinates = ref([]);
 const maxDistance = ref(0);
 const groupColors = ref([]);
-const Layers = ref([]);
+const PolygonLayers = ref([]);
+const LineLayers = ref([]);
 
 function createCoordinates(coords) {
   const { lng, lat } = coords;
@@ -74,7 +76,7 @@ function generateColors(group) {
 }
 
 // click to change the color of a group
-function changeColor(index) {
+function changeColor({ index, length }) {
   const color = randomColor();
   if (groupColors.value.includes(color)) {
     return changeColor(index);
@@ -82,29 +84,53 @@ function changeColor(index) {
   groupColors.value[index] = color;
 
   //change fill color
-  map.value.setPaintProperty(
-    `${Layers.value[index].name}-fill`,
-    "fill-color",
-    color
-  );
+  if (length > 2) {
+    map.value.setPaintProperty(
+      `${PolygonLayers.value[index].name}-fill`,
+      "fill-color",
+      color
+    );
 
-  //change line color
-  map.value.setPaintProperty(
-    `${Layers.value[index].name}-line`,
-    "line-color",
-    color
-  );
+    //change line color
+    map.value.setPaintProperty(
+      `${PolygonLayers.value[index].name}-line`,
+      "line-color",
+      color
+    );
 
-  // need points to place the markers
-  const points = Layers.value[index].points;
+    // need points to place the markers
+    const points = PolygonLayers.value[index].points;
 
-  // change Marker color
-  const Marker = Layers.value[index].marker.map((marker, index) => {
-    marker.remove();
-    return createMarker(color, points[index]);
-  });
+    // change Marker color
+    const Marker = PolygonLayers.value[index].marker.map((marker, index) => {
+      marker.remove();
+      return createMarker(color, points[index]);
+    });
 
-  Layers.value[index].marker = Marker;
+    PolygonLayers.value[index].marker = Marker;
+
+    return;
+  } else {
+    // /change line color
+    map.value.setPaintProperty(
+      `${LineLayers.value[index - polygonCoordsLength.value].name}-line`,
+      "line-color",
+      color
+    );
+
+    // need points to place the markers
+    const points = LineLayers.value[index - polygonCoordsLength.value].points;
+
+    // change Marker color
+    const Marker = LineLayers.value[
+      index - polygonCoordsLength.value
+    ].marker.map((marker, idx) => {
+      marker.remove();
+      return createMarker(color, points[idx]);
+    });
+
+    LineLayers.value[index - polygonCoordsLength.value].marker = Marker;
+  }
 }
 
 // form array to be used for mapbox layers
@@ -119,18 +145,29 @@ function generateMapLines(groupCordinates) {
     mapLines.push({ polygonCoords, name, points });
   });
 
-  return (
-    mapLines
-      // we filter becuase you can form any shape without at least 4 points
+  // we filter because you can form any shape without at least 4 points
+
+  return {
+    polygons: mapLines
       .filter((item) => item.polygonCoords.length > 3)
       .map((item) => ({
         name: item.name,
         polygonCoords: polygon([item.polygonCoords]),
         points: item.points,
-      }))
-  );
+      })),
+    lines: mapLines
+      .filter(
+        (item) => item.polygonCoords.length < 4 && item.polygonCoords.length > 2
+      )
+      .map((item) => ({
+        name: item.name,
+        polygonCoords: lineString(item.polygonCoords),
+        points: item.points,
+      })),
+  };
 }
 
+//create marker for each point
 function createMarker(color, lngLat) {
   const el = document.createElement("div");
   el.className = "marker";
@@ -139,11 +176,79 @@ function createMarker(color, lngLat) {
   return new mapboxgl.Marker(el).setLngLat(lngLat).addTo(map.value);
 }
 
+// draw Map lines
+
 function drawLines(groupCordinates) {
   const mapLines = generateMapLines(groupCordinates);
   // loop though each group and draw the lines
 
-  mapLines.forEach((line, index) => {
+  const { polygons, lines } = mapLines;
+
+  // create polygons
+  attachPolygonData(polygons);
+
+  // create lines
+  attachLineData(lines);
+}
+
+function attachLineData(lines) {
+  // null check
+  if (lines.length === 0) {
+    if (LineLayers.value.length === 0) {
+      return;
+    }
+    // else remove all lines
+    LineLayers.value.forEach((layer) => {
+      layer.marker.forEach((marker) => marker.remove());
+    });
+    LineLayers.value = [];
+  }
+
+  // get line data
+
+  lines.forEach((line, index) => {
+    const { polygonCoords, name, points } = line;
+    const marker = points.map((point) => {
+      // create marker
+      return createMarker(
+        groupColors.value[index + (groupCordinates.value.length - 1)],
+        point
+      );
+    });
+    // add polygoncoords as source
+    const source = polygonCoords;
+
+    if (map.value.getSource(name)) {
+      return map.value.getSource(name).setData(source);
+    }
+    // add source to map
+    map.value.addSource(name, {
+      type: "geojson",
+      data: source,
+    });
+
+    let lineLayer = null;
+
+    //create layer
+    lineLayer = {
+      id: `${name}-line`,
+      type: "line",
+      source: name,
+      paint: {
+        "line-width": 2,
+        "line-color": groupColors.value[groupCordinates.value.length - 1],
+      },
+    };
+    // add line layer
+    map.value.addLayer(lineLayer);
+
+    //push details into layers so you can track
+    LineLayers.value.push({ name, lineLayer, marker, points });
+  });
+}
+
+function attachPolygonData(polygon) {
+  polygon.forEach((line, index) => {
     // get line data
     const { polygonCoords, name, points } = line;
 
@@ -163,8 +268,11 @@ function drawLines(groupCordinates) {
       data: source,
     });
 
+    let lineLayer = null;
+    let fillLayer = null;
+
     //create layer
-    const linelayer = {
+    lineLayer = {
       id: `${name}-line`,
       type: "line",
       source: name,
@@ -173,8 +281,10 @@ function drawLines(groupCordinates) {
         "line-color": groupColors.value[index],
       },
     };
+    // add line layer
+    map.value.addLayer(lineLayer);
 
-    const fillLayer = {
+    fillLayer = {
       id: `${name}-fill`,
       type: "fill",
       source: name,
@@ -183,29 +293,31 @@ function drawLines(groupCordinates) {
         "fill-opacity": 0.4,
       },
     };
-    // add line layer
-    map.value.addLayer(linelayer);
 
     //add fill layer to map
     map.value.addLayer(fillLayer);
+
     //push details into layers so you can track
-    Layers.value.push({ name, linelayer, fillLayer, marker, points });
+    PolygonLayers.value.push({ name, lineLayer, fillLayer, marker, points });
   });
 }
 
+// group coordinates based on distance
+
 const groupCordinates = computed(() => {
   let coordinateCopy = [...coordinates.value];
-  // if (coordinateCopy.length === 0) {
-  //   return [];
-  // }
-  // if ((coordinateCopy.length > 0) & (coordinateCopy.length < 9)) {
-  //   return [
-  //     {
-  //       name: "",
-  //       group: coordinateCopy,
-  //     },
-  //   ];
-  // }
+
+  if (coordinateCopy.length === 0) return [];
+
+  if (coordinateCopy.length > 0 && coordinateCopy.length < 9) {
+    return [
+      {
+        group: coordinateCopy,
+        name: "Ungrouped",
+      },
+    ];
+  }
+
   let groups = [];
 
   // loop through the coordinates and group them by distance
@@ -217,7 +329,7 @@ const groupCordinates = computed(() => {
         [coordinateCopy[j].lng, coordinateCopy[j].lat]
       );
       // if less than max distance then add to a group
-      if (distance < maxDistance.value) {
+      if (distance <= maxDistance.value) {
         group.push(coordinateCopy[j]);
       }
     }
@@ -237,29 +349,25 @@ const groupCordinates = computed(() => {
     }
   }
 
-  return groups;
-});
-
-//grouped items
-
-const groupedPoints = computed(() => {
-  return groupCordinates.value.filter((item) => item.group.length >= 2);
-});
-
-const ungroupedPoints = computed(() => {
-  const group = groupCordinates.value
-    .filter((item) => item.group.length < 2)
+  const grouped = groups.filter((item) => item.group.length > 1);
+  const ungrouped = groups
+    .filter((item) => item.group.length === 1)
     .flatMap((item) => item.group);
 
-  return {
-    name: "Ungrouped",
-    group,
-  };
+  let combined;
+  if (ungrouped.length > 0) {
+    combined = [...grouped, { name: "Ungrouped", group: ungrouped }];
+  } else {
+    combined = [...grouped];
+  }
+
+  return combined;
 });
 
-function threeDp(value) {
-  return value.toFixed(3);
-}
+//check length of polygons coords
+const polygonCoordsLength = computed(() => {
+  return groupCordinates.value.filter((item) => item.group.length > 2).length;
+});
 
 function clearPoint() {
   // clear markers
@@ -270,11 +378,21 @@ function clearPoint() {
   coordinates.value = [];
 
   //remove source
-  Layers.value.forEach((layer) => {
+  PolygonLayers.value.forEach((layer) => {
     map.value.removeLayer(`${layer.name}-line`);
     map.value.removeLayer(`${layer.name}-fill`);
     map.value.removeSource(layer.name);
     layer.marker.forEach((point) => point.remove());
+  });
+}
+
+function changePointName(pointName) {
+  const index = coordinates.value.findIndex((item) => item.name === pointName);
+
+  // console.log(index);
+  coordinates.value[index].name = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    separator: " ",
   });
 }
 
@@ -298,7 +416,7 @@ onMounted(() => {
 //watchers
 
 watch(
-  () => groupedPoints.value,
+  () => groupCordinates.value,
   (newCoordinates) => {
     // if coordinate become more than 9 generate group colors and draw lines
     if (newCoordinates.length > 1) {
@@ -331,31 +449,15 @@ export default {
       <div class="flex-grow flex flex-col">
         <div class="mb-[14px] text-[20px] leading-[24px]">Groups</div>
         <div class="h-[579px] overflow-y-auto">
-          <div
-            v-for="(coord, index) in groupedPoints"
+          <GroupComponent
+            :coord="coord"
+            :index="index"
+            :group-colors="groupColors"
+            v-for="(coord, index) in groupCordinates"
             :key="index + coord.name"
-            class="mb-4"
-          >
-            <button
-              class="flex items-center gap-1 hover:bg-gray-200 p-1 rounded"
-              @click="changeColor(index)"
-            >
-              <div
-                class="h-3 w-3 rounded-[2px]"
-                :style="`background-color: ${groupColors[index]}`"
-                v-if="coord.name !== 'Ungrouped'"
-              ></div>
-              <div class="flex h-full items-center pt-1">Group</div>
-            </button>
-            <div v-for="group in coord.group" :key="group.name">
-              <span>
-                {{ group.name }}
-              </span>
-              <span class="text-[#767676] pl-2"
-                >{{ threeDp(group.lng) }} {{ threeDp(group.lat) }}</span
-              >
-            </div>
-          </div>
+            @changeColor="changeColor"
+            @changePointName="changePointName"
+          />
         </div>
       </div>
 
---
+++ src/components/GroupComponent.vue
@@ -0,0 +1,59 @@
+<script setup>
+const emit = defineEmits(["changeColor", "changePointName"]);
+defineProps({
+  coord: {
+    type: Object,
+    required: true,
+  },
+  index: {
+    type: Number,
+    required: false,
+  },
+  groupColors: {
+    type: Array,
+    required: true,
+  },
+});
+
+function threeDp(value) {
+  return value.toFixed(3);
+}
+
+function changeColor(index, length) {
+  emit("changeColor", { index, length });
+}
+
+function changePointName(name) {
+  emit("changePointName", name);
+}
+</script>
+
+<template>
+  <div class="mb-4">
+    <button
+      class="flex items-center gap-1 hover:bg-gray-200 p-1 rounded"
+      @click="changeColor(index, coord.group.length)"
+    >
+      <div
+        class="h-3 w-3 rounded-[2px]"
+        :style="`background-color: ${groupColors[index]}`"
+        v-if="coord.name !== 'Ungrouped'"
+      ></div>
+      <div class="flex h-full items-center pt-1">
+        {{ coord.name !== "Ungrouped" ? "Group" : coord.name }}
+      </div>
+    </button>
+    <div v-for="group in coord.group" :key="group.name" class="pl-1">
+      <span
+        role="button"
+        class="hover:bg-gray-200 rounded"
+        @click="changePointName(group.name)"
+      >
+        {{ group.name }}
+      </span>
+      <span class="text-[#767676] pl-2"
+        >{{ threeDp(group.lng) }} {{ threeDp(group.lat) }}</span
+      >
+    </div>
+  </div>
+</template>
